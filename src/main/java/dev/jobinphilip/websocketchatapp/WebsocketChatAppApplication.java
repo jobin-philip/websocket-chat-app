b5460d5b2456166bc37b36d1269839255bf76dd1
Jobin Philip <jobinphilip68@gmail.com>
2023-08-11 21:28:01 +0530

    Initialized project using Spring Initializr

---
+++ src/main/java/dev/jobinphilip/websocketchatapp/WebsocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package dev.jobinphilip.websocketchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatAppApplication.class, args);
+	}
+
+}
